ce48a3ddc6ee85411683141f18dd5fe8c02e1153
KAZIM586 <159234933+KAZIM586@users.noreply.github.com>
2024-03-26 22:04:33 +0530

    sum program in loop

---
+++ loop/sum.py
@@ -0,0 +1,19 @@
+# while loop
+
+i=100
+sum=0
+while i<200:
+    if i%2==0:
+     sum=sum+i
+    i=i+1
+print(sum)
+      
+
+
+# for loop
+
+# sum=0
+# for i in range(100,200,1):
+#     if i%2==0:
+#       sum=sum+i
+# print(sum)
