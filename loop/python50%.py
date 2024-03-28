987450f9383d7a24d89de9722ee0266bf7215fa4
KAZIM586 <159234933+KAZIM586@users.noreply.github.com>
2024-03-28 23:32:37 +0530

    loop  program

---
+++ loop/python50%.py
@@ -0,0 +1,325 @@
+# import time
+
+# def wait_for_bus():
+#     while True:
+#         print("Still waiting for the bus...")
+#         time.sleep(10)  # Wait for 10 seconds
+
+#         response = input("Has the bus arrived? (yes/no): ").lower()
+
+#         if response == "yes":
+#             print("Hooray! The bus has arrived. Have a safe journey!")
+#             break
+#         elif response == "no":
+#             print("Okay, continue waiting for the bus...")
+#         else:
+#             print("Invalid input. Please enter 'yes' or 'no'.")
+
+# if __name__ == "__main__":
+#     wait_for_bus()
+
+# x=4+5/5*2
+# print(x)
+
+# string="Guess the output of this code"
+# print(string.split('t'))
+
+
+# a=4
+# b=5
+# if(a>b):
+#     print('a>b')
+# elif(a<b and a==b):
+#     print('a<=b')
+
+# a=4
+# b=5
+# if(a>b):
+#     print('a>b')
+# elif:
+#     print('a<=b')
+
+
+# a=4
+# b=5
+# if(a>b):
+#     print('a>b')
+# elif(a<b):
+#     print('a<b')
+# else: print('a==b')
+
+# if('p' in 'Python'):
+#     print('P is present',end="" "")
+# else:
+#     print('Not present')
+
+
+# i = 0
+# while i < 3:
+#     print(i)
+#     i += 1
+# else:
+#     print(0)
+
+# a =1 
+# while True:
+#  if a %7 == 0:
+#   break
+# print(a,end=")
+# a += 1
+
+
+# for i in range(1,6,2):
+#     if (i==4):
+#         break
+#     print(i,end=" ")
+
+
+# list = [
+
+# ['a', 'b', ['c', 10]],
+
+# ['p', [1,2,3], 'q', 'r'],
+
+# [7,8,9,['x', 'y', 'z']]
+
+# ]
+
+# print(list[1][1][2], list[0][2][0], list[2][3][1])
+
+
+
+# a=10
+# b=4
+# if(a&b):
+#     print((a-b)/2)
+# else:
+#     print(a-b/2)
+
+# a=10
+# b=4
+# if(a^b):
+#     print((a-b)/2)
+# else:
+#     print(a-b/2)
+
+# a=10
+# b=4
+# if(~a):
+#     print(a-2*b+1)
+# else:
+#     print(a-b/2)
+
+
+# a=10
+# b=4
+# if(a-b):
+#     print(a-2*(b+1))
+# else:
+#     print(a-b/2)
+
+
+# i =0 
+# while i< 5:
+#   print(i,end="")
+# i += 15.
+# if i == 3:
+# break
+# else:
+#  print(0)
+
+
+# if(" or 5 and True"):
+#     print("Hello",end=" ")
+# else:
+#     print('Hi',end=" ")
+# print('!')
+
+
+# set(range(3,3))
+# set(range(1,-3,-5))
+# set(range(-2,4,3))
+
+
+
+# for i in ('a','b','c','d'):
+#     print(i,end="")
+# else:
+#     print('end')
+
+
+# x=4+3 and 6 or 4>5+2
+# print(x)
+
+# str1=str(1234)
+# str2='1234'
+# print(str1==str2,end=" ")
+# print(str1 is str2)
+
+
+# for i in range(5):
+#     if(i%2):print(i,end=" ")
+
+
+# print(int(4.9))
+
+
+# opt=2
+# if(opt==1):
+#     print('Choosen first option')
+# elif(opt==2):
+#     print("Choosen second option")
+# elif(opt==3):
+#     print('Choosen third option')
+
+
+# if(1): print("If block")
+# else: print("Else block")
+
+
+# no=5
+# if(no%2==0):
+#     print('Even Number')
+# else(no%2==1):
+#     print('Odd Number')
+
+
+# i=0
+# while (i<10):
+#     if(i%2==0): 
+#         i=i+3
+#     i=i+1
+
+
+# print(2**1*3,end=" ")
+# print(2*1**3)
+
+# x= 5+(6>4)+6/3*5
+# print(x)
+
+
+# n=5
+# while(n):
+#     n=n-1
+#     print(n,end=' ')
+
+
+# x =1
+# while True:
+#  if x %5 == 0:
+#   break 
+#  print(x,end="")
+#  x += 1
+
+
+# print('5.0'==5.0)
+# print(5.0==5)
+# print(5.1>5)
+
+
+
+# if("false"):
+#     print("false")
+# elif("true"):
+#     print("true")
+# else:
+#     print("None")
+
+
+# x = 5
+
+# y = x % 2==0
+
+# print(type(y))
+
+
+# c=0
+# for i in 'abc xyz':
+#     c=c+1
+# print(c)
+
+
+# print(5%2==2, end = " ")
+# print(5.5%2==1)
+
+
+# z ="xyz" 
+# j ="j" 
+# while j in z:
+#  print(j,end=" ")
+
+
+# a=4
+# if a<0:
+#     print("Negative")
+# print(a)
+
+
+# a=5
+# b=(a==2)
+# print(b+3)
+
+
+# a=4
+# b=5
+# if(a>b):
+#     print('a>b')
+# elif(a<b and a==b):
+#     print('a<=b')
+
+# a=4
+# b=5
+# if(a>b):
+#     print('a>b')
+# elif:
+#     print('a<=b')
+
+# print(9.1>'9.1')
+# print('9'>'9.1')
+# print(9!='9')
+
+
+
+# x=5
+# y=3
+# z=2
+# if(y-z>5-y):
+#     y=y+3
+# elif(z-x):
+#     z=z+4
+# else:
+#     x=x-1
+# print(x+y*2-z)
+
+
+
+# a=4
+# b=5
+# if(a>b):
+#     print('a>b')
+# elif(a<b):
+#     print('a<b')
+# else: print('a==b')
+
+
+
+
+# x =1
+# while True:
+#   if x %5 == 0:
+#    break 
+# print(x,end="")
+# x +=1
+
+
+# for i in ('a','b','c','d'):
+#     print(i,end="")
+# else:
+#     print('end')
+
+# a =1 
+# while True:
+#  if a %7 == 0:
+# #   break
+#   print(a,end="")
+#   a += 1
