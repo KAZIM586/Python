5abef91bd0c11c97b1d57e3181dff31a42cfc6b5
KAZIM586 <159234933+KAZIM586@users.noreply.github.com>
2024-04-10 22:54:24 +0530

    union program

---
+++ union.py
@@ -0,0 +1,36 @@
+#Union
+A={1,2,3}
+B={4,5,6}
+print(A|B)
+print(A.union(B))
+
+
+
+# intersection
+
+A={1,2,3,4}
+B={1,2,5,6,8}
+print(A&B)
+print(A.intersection(B))
+
+# Difference
+
+A={1,2,3}
+B={1,2,6}
+print(A-B)
+print(A.difference(B))
+
+
+
+
+# Symmetric_difference
+
+A={1,2,3,4,5}
+B={1,2,3,4,5,6,78}
+print(A^B)
+print(A.symmetric_difference(B))
+
+
+
+
+
