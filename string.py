986048fbb4aa854d4e088376f8cbcba1879377a3
KAZIM586 <159234933+KAZIM586@users.noreply.github.com>
2024-04-14 22:05:31 +0530

    String in python

---
+++ string.py
@@ -0,0 +1,26 @@
+# String
+
+# 1 2 3 4 5 6 7 8 
+# I T V E D A N T
+# -8-7-6-5-4-3-2-1
+
+
+text = "ITVEDANT"
+  
+print(text[-3:-1])
+
+str1 = "Hello ji "
+
+str2 = "ka hal"
+
+print(str1+str2)
+
+
+# formating
+
+st_name = "Mohan"
+
+st_marks = 98
+
+str1 = "The student name is {s} and marks is {m}".format(s=st_name, m=st_marks)
+print(str1)
