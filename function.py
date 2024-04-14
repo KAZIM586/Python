dcffe37b4ca23905694802beee038108d3bc5b0e
KAZIM586 <159234933+KAZIM586@users.noreply.github.com>
2024-04-14 22:03:44 +0530

    function in python

---
+++ function.py
@@ -0,0 +1,23 @@
+# Function is a reusable block of code to perform specific task
+
+output={True,True,True,True,False}
+l=["monu","nonu",664666,0]
+# print(all(l))   #all()
+# print(any(l))   #any()
+a= True  #1
+b= True    #1
+# print(a+b)
+c= True     #1
+d= False     #0
+# print(c+d)
+
+l=[1,2,3,45,8,9]
+# print(min(l))
+# print(max(l))
+# print(sorted(l))
+
+
+fruits=['mango','apple','mango','grapes']
+result=enumerate(fruits)   # it give memory refrence value 
+print(result)
+print(set(result))
